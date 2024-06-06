f3f1b1609b53a9424fd8d9a92522e221ad84aa16
Enricco Gemha <enriccogemha@gmail.com>
2024-06-05 22:01:25 -0300

    chore: Refactor read_vrp_input function to improve readability and error handling

--- src/implementations/utils.cpp
+++ src/implementations/utils.cpp
@@ -4,42 +4,43 @@
 #include <sstream>
 
 void read_vrp_input(const std::string& filename, int& num_nodes, std::map<int, int>& demands, std::vector<Edge>& edges) {
-    std::ifstream infile(filename);
-    std::string line;
+    std::ifstream infile(filename);  // Open the file
+    std::string line;                // Line to read the file
 
+    // Check if the file is alreadyopen
     if (!infile.is_open()) {
         std::cerr << "Could not open the file: " << filename << std::endl;
         return;
     }
 
-    // Read the number of nodes
-    std::getline(infile, line);
-    std::istringstream iss_num_nodes(line);
-    iss_num_nodes >> num_nodes;
+    // Read the number of nodes (first line of the file)
+    std::getline(infile, line);             // Read the first line
+    std::istringstream iss_num_nodes(line); // Convert the line to a stream
+    iss_num_nodes >> num_nodes;             // Read the number of nodes
 
     // Read the demands
     for (int i = 0; i < num_nodes - 1; ++i) {
-        std::getline(infile, line);
-        std::istringstream iss(line);
-        int node, demand;
-        iss >> node >> demand;
-        demands[node] = demand;
+        std::getline(infile, line);   // Read the line
+        std::istringstream iss(line); // Convert the line to a stream
+        int node, demand;             // Variables to store the node and demand
+        iss >> node >> demand;        // Read the node and demand
+        demands[node] = demand;       // Store the demand
     }
 
     // Read the number of edges
-    std::getline(infile, line);
-    int num_edges;
-    std::istringstream iss_num_edges(line);
-    iss_num_edges >> num_edges;
+    std::getline(infile, line);              // Read the line
+    int num_edges;                           // Variable to store the number of edges
+    std::istringstream iss_num_edges(line);  // Convert the line to a stream
+    iss_num_edges >> num_edges;              // Read the number of edges
 
-    // Read the edges
+    // Read the edges of the graph
     for (int i = 0; i < num_edges; ++i) {
-        std::getline(infile, line);
-        std::istringstream iss(line);
-        int from, to, weight;
-        iss >> from >> to >> weight;
-        edges.push_back({from, to, weight});
+        std::getline(infile, line);           // Read the line
+        std::istringstream iss(line);         // Convert the line to a stream
+        int from, to, weight;                 // Variables to store the from, to, and weight
+        iss >> from >> to >> weight;          // Read the from, to, and weight
+        edges.push_back({from, to, weight});  // Store the edge
     }
 
-    infile.close();
+    infile.close();  // Close the file
 }
